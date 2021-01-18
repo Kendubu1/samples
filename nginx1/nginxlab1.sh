2b42929467c2af76af167d144623e50f3caba6e5
Kendrick Dubuisson <kedubuis@microsoft.com>
2021-01-18 01:57:29 -0600

    udpated logging

--- nginx1/nginxlab1.sh
+++ nginx1/nginxlab1.sh
@@ -1,25 +1,26 @@
 #!bin/sh
 
-#Startlogging
+echo "#Startlogging"
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>scriptlog.out 2>&1
 
-#Install Nginx
+echo "#Install Nginx"
 sudo apt-get update 
 sudo apt install nginx -y
 systemctl status nginx
 
-#Install Nodejs, Yarn, PM2 & Dependencies
+echo "#Install Nodejs, Yarn, PM2 & Dependencies"
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - 
 
-sudo apt-get install -y nodejs 
+sudo apt install -y nodej
+sudo apt install npm -y
 sudo apt install yarn -y
 sudo npm install -g pm2 -y
 
-#Install App & Start w/ NPM | PM@
+echo "#Install App & Start w/ NPM | PM2"
 cd /opt/ 
 sudo mkdir app
 cd app
@@ -28,7 +29,7 @@ sudo npm install
 #(npm start&)
 #pm2 start src/index.js 
 
-# Configure Nginx 
+echo "# Configure Nginx" 
 export PUBLIC_IP=$(curl -s http://checkip.amazonaws.com || printf "0.0.0.0")
 sudo sed -i "s/worker_processes.*;/worker_processes 1;/" /etc/nginx/nginx.conf
 sudo sed -i "s/worker_connections.*;/worker_connections 3;/" /etc/nginx/nginx.conf
@@ -44,7 +45,7 @@ sudo sh -c "printf '%s\n' '' \
     '' > /etc/nginx/conf.d/node.conf"
 sudo nginx -s reload
 
-#Configure & Start Node Service
+echo "#Configure & Start Node Service"
 #sudo printf '%s\n' '#!bin/bash' 'pm2 start /opt/app/src/index.js' > /opt/app/node.sh
 sudo printf '%s\n' '#!bin/bash' 'cd /opt/app' 'npm start' > /opt/app/node.sh
 sudo chmod +x /opt/app/node.sh
