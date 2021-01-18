727355e9bfa72f93285a4db9b7968b4fa1cb03fb
Kendrick Dubuisson <kedubuis@microsoft.com>
2021-01-17 22:15:23 -0600

    adding node service

--- nginx1/nginxlab.sh
+++ nginx1/nginxlab.sh
@@ -1,35 +1,32 @@
 #!bin/sh
-#Install Nginx
 
-echo "### Installing Nginx ###"
+#Startlogging
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>scriptlog.out 2>&1
+
+#Install Nginx
+sudo apt-get update 
 sudo apt install nginx -y
 systemctl status nginx
 
-echo "##### Installing NodeJS and Yarn #####" 
+#Install Nodejs, Yarn, PM2 & Dependencies
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-
-# Installing dependencies
-sudo apt-get update 
-# Installing nodejs
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - 
+
 sudo apt-get install -y nodejs 
-
-# Installing yarn
 sudo apt install yarn -y
+sudo npm install -g pm2 -y
 
-# Install PM2
-sudo npm install pm2 -y
-
-#Install/Start Application
-export PORT=3000
-cd /home/
-sudo git clone https://github.com/Kendubu1/covid-19-demo-express-js-app.git
-cd covid-19-demo-express-js-app/
+#Install App & Start w/ NPM | PM@
+cd /opt/ 
+sudo mkdir app
+cd app
+sudo git clone https://github.com/Kendubu1/covid-19-demo-express-js-app.git .
 sudo npm install 
-npm start
-
-## INSTALL W/ PM2
+#(npm start&)
+#pm2 start src/index.js 
 
 # Configure Nginx 
 export PUBLIC_IP=$(curl -s http://checkip.amazonaws.com || printf "0.0.0.0")
@@ -45,5 +42,24 @@ sudo sh -c "printf '%s\n' '' \
     '    proxy_pass http://localhost:3000/;' \
     '     }' \
     '     }' \  
-    '' > test.txt"
+    '' > test.txt" exit
 sudo nginx -s reload
+
+#Configure & Start Node Service
+sudo printf '%s\n' '#!bin/bash' 'pm2 start /opt/app/src/index.js' > /opt/app/node.sh
+sudo chmod +x /opt/app/node.sh
+sudo printf '%s\n' '' \
+    '[Unit]' \
+    'Description=node pm2 start script' \
+    ''\
+    '[Service]' \
+    'ExecStart=/opt/app/node.sh' \
+    ''\
+    '[Install]' \
+    'WantedBy=multi-user.target' \
+    '' > /etc/systemd/nodepm2.service
+
+cp /etc/systemd/nodepm2.service /lib/systemd/system
+sudo systemctl enable nodepm2.service
+sudo systemctl start nodepm2
+
