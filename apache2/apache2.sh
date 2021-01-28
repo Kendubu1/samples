a2d4bebd4d483fcbd4827a0e2502def644204a97
Kendrick Dubuisson <kedubuis@microsoft.com>
2021-01-28 12:31:04 -0600

    update apache sh

--- apache2/apache2.sh
+++ apache2/apache2.sh
@@ -1,9 +1,9 @@
 #!bin/sh
 
 #Startlogging"
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>scriptlog.out 2>&1
+# exec 3>&1 4>&2
+# trap 'exec 2>&4 1>&3' 0 1 2 3
+# exec 1>scriptlog.out 2>&1
 
 #Install Apache"
 sudo apt-get update
