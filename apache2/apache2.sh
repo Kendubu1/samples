000a12a05ffabdce50b22308991d9cf096b24422
Kendrick Dubuisson <kedubuis@microsoft.com>
2021-01-28 14:42:23 -0600

    rollback

--- apache2/apache2.sh
+++ apache2/apache2.sh
@@ -24,7 +24,8 @@ sudo ufw allow in "Apache Full"
 #Update permissions & install app
 sudo rm -rf /var/www/html/
 sudo mkdir /var/www/html/
-cd /var/www/html/ && sudo git clone https://github.com/Kendubu1/commtest.git .
+sudo cd /var/www/html/ 
+sudo git clone https://github.com/Kendubu1/commtest.git .
 sudo chmod -R 0755 /var/www/html/
 
 sleep 15
@@ -43,23 +44,24 @@ sudo apt install php libapache2-mod-php php-mysql -y
 sudo sed -i "s/DirectoryIndex.*/DirectoryIndex index.php index.html/" /etc/apache2/mods-enabled/dir.conf
 
 #Update Update/disable Prefork
-sudo a2dismod php7.2
-sudo a2dismod mpm_prefork
-sudo a2dismod mpm_worker
+# sudo a2dismod php7.2
+# sudo a2dismod mpm_prefork
+# sudo a2dismod mpm_worker
+# sudo a2enmod php7.2
 
-#note:have sed skip first occurrance 
-sudo sed -i "s/StartServers.*/StartServers 1/" /etc/apache2/mods-available/mpm_prefork.conf 
-sudo sed -i "s/MinSpareServers.*/MinSpareServers 1/" /etc/apache2/mods-available/mpm_prefork.conf
-sudo sed -i "s/MaxSpareServers.*/MaxSpareServers 1/" /etc/apache2/mods-available/mpm_prefork.conf
-sudo sed -i "s/MaxRequestWorkers.*/MaxRequestWorkers 1/" /etc/apache2/mods-available/mpm_prefork.conf
-sudo sed -i "s/MaxConnectionsPerChild.*/MaxConnectionsPerChild 1/" /etc/apache2/mods-available/mpm_prefork.conf
+# #note:have sed skip first occurrance 
+# sudo sed -i "s/StartServers.*/StartServers 1/" /etc/apache2/mods-available/mpm_prefork.conf 
+# sudo sed -i "s/MinSpareServers.*/MinSpareServers 1/" /etc/apache2/mods-available/mpm_prefork.conf
+# sudo sed -i "s/MaxSpareServers.*/MaxSpareServers 1/" /etc/apache2/mods-available/mpm_prefork.conf
+# sudo sed -i "s/MaxRequestWorkers.*/MaxRequestWorkers 1/" /etc/apache2/mods-available/mpm_prefork.conf
+# sudo sed -i "s/MaxConnectionsPerChild.*/MaxConnectionsPerChild 1/" /etc/apache2/mods-available/mpm_prefork.conf
 
-sudo sed -i "s/StartServers.*/StartServers 1/" /etc/apache2/mods-available/mpm_worker.conf
-sudo sed -i "s/MinSpareThreads.*/MinSpareThreads 1/" /etc/apache2/mods-available/mpm_worker.conf
-sudo sed -i "s/MaxSpareThreads.*/MaxSpareThreads 1/" /etc/apache2/mods-available/mpm_worker.conf
-sudo sed -i "s/ThreadLimit.*/ThreadLimit 1/" /etc/apache2/mods-available/mpm_worker.conf
-sudo sed -i "s/ThreadsPerChild.*/ThreadsPerChild 1/" /etc/apache2/mods-available/mpm_worker.conf
-sudo sed -i "s/MaxRequestWorkers.*/MaxRequestWorkers 1/" /etc/apache2/mods-available/mpm_worker.conf
-sudo sed -i "s/MaxConnectionsPerChild.*/MaxConnectionsPerChild 1/" /etc/apache2/mods-available/mpm_worker.conf
+# sudo sed -i "s/StartServers.*/StartServers 1/" /etc/apache2/mods-available/mpm_worker.conf
+# sudo sed -i "s/MinSpareThreads.*/MinSpareThreads 1/" /etc/apache2/mods-available/mpm_worker.conf
+# sudo sed -i "s/MaxSpareThreads.*/MaxSpareThreads 1/" /etc/apache2/mods-available/mpm_worker.conf
+# sudo sed -i "s/ThreadLimit.*/ThreadLimit 1/" /etc/apache2/mods-available/mpm_worker.conf
+# sudo sed -i "s/ThreadsPerChild.*/ThreadsPerChild 1/" /etc/apache2/mods-available/mpm_worker.conf
+# sudo sed -i "s/MaxRequestWorkers.*/MaxRequestWorkers 1/" /etc/apache2/mods-available/mpm_worker.conf
+# sudo sed -i "s/MaxConnectionsPerChild.*/MaxConnectionsPerChild 1/" /etc/apache2/mods-available/mpm_worker.conf
 
-sudo systemctl restart apache
+# sudo systemctl restart apache2
